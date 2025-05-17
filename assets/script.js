b23e179652102a9c2aff32096aa877937fdde257
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-05-17 23:29:43 +0545

    Add hover effect to the squares to darken them
    Add hover effect to the squares to darken them on each hover until it
    fully reveals the black background of the container within only 10
    interactions.

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,15 @@ for(let i=1; i<=totalNumberOfSquare; i++){
     width:${squareWidth}px;
     height:${squareHeight}px;
   `;
-  
+
+  //Add Event-listener
+  square.addEventListener("mouseover", () =>{
+    let currentOpacity = parseFloat(window.getComputedStyle(square).opacity);
+    if (currentOpacity>0) {
+      //Ensures minimum value is always 0
+      currentOpacity = Math.max(0, currentOpacity-0.1);
+      square.style.opacity = currentOpacity;
+    }
+  });
   container.appendChild(square);
 }
