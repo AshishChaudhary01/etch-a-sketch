620eb1530118fdfbbb1216d66c7758b784eec02f
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-05-19 20:13:34 +0545

    Add feature to create a new grid, randomize color
    Add a button on top of the grid to prompt user for the new grid size.
    Add feature to randomize the background-color of the square when hovered
    over it.

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,63 @@ const rowCount = columnCount = 16;
 const totalNumberOfSquare = rowCount*columnCount;
 const squareHeight = squareWidth = container.clientWidth/16;
 
-for(let i=1; i<=totalNumberOfSquare; i++){
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.style.cssText= `
-    width:${squareWidth}px;
-    height:${squareHeight}px;
-  `;
+function createNewGrid(squarePerSideCount){
+  let squareHeight = container.clientHeight/squarePerSideCount;
+  let squareWidth = container.clientWidth/squarePerSideCount;
 
-  //Add Event-listener
-  square.addEventListener("mouseover", () =>{
-    let currentOpacity = parseFloat(window.getComputedStyle(square).opacity);
-    if (currentOpacity>0) {
-      //Ensures minimum value is always 0
-      currentOpacity = Math.max(0, currentOpacity-0.1);
-      square.style.opacity = currentOpacity;
+  //Create Squares
+  totalNumberOfSquare = squarePerSideCount**2;
+  for(let i=1; i<=totalNumberOfSquare; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.cssText= `
+      width:${squareWidth}px;
+      height:${squareHeight}px;
+    `;
+
+    //Add Hover Effects
+    square.addEventListener("mouseover", () =>{
+      //Set random RGB Value for the square
+      square.style.backgroundColor = getRandomRgbValue();
+      let currentOpacity = parseFloat(window.getComputedStyle(square).opacity);
+      if (currentOpacity>0) {
+        //Ensures minimum value is always 0
+        currentOpacity = Math.max(0, currentOpacity-0.1);
+        square.style.opacity = currentOpacity;
+      }
+    });
+    container.appendChild(square);
+  }
+}
+
+createNewGrid(squarePerSideCount);
+
+const gridSizeBtn = document.querySelector("#grid-size-btn");
+
+//Event listener to create new grid
+gridSizeBtn.addEventListener("click", () => {
+  let message = "";
+  while(true){
+    squarePerSideCount = promptUser(message);
+    if(squarePerSideCount>0 && squarePerSideCount<=100){
+      container.innerHTML = "";
+      createNewGrid(squarePerSideCount);
+      break;
     }
-  });
-  container.appendChild(square);
+    else {
+      message = "Invalid Input! Please try again. ";
+    }
+  }
+});
+
+//Function to prompt the user for the number of squares per side
+function promptUser(message=""){
+  return parseInt(prompt(message + "Enter the number of squares in a side for the new grid[1-100]: "));
+}
+
+function getRandomRgbValue(){
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
